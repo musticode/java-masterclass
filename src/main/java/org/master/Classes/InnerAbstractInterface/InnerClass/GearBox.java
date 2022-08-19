4d9aecdd46ec3f4303c2c0c969c29a9788320b30
mustidrummer <mustafakaratas4179@gmail.com>
2022-08-19 18:03:07 +0300

    innerclass started

---
+++ src/main/java/org/master/Classes/InnerAbstractInterface/InnerClass/GearBox.java
@@ -0,0 +1,13 @@
+package org.master.Classes.InnerAbstractInterface.InnerClass;
+
+import java.util.ArrayList;
+
+public class GearBox {
+//    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int gearNumber = 0;
+
+    public GearBox(){
+
+    }
+}
