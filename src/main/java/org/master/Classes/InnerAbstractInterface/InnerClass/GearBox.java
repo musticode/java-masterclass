36164915f633449fcd9a70fa2727d643e1468571
mustafaka <mustafakaratas4179@gmail.com>
2022-08-22 16:54:13 +0300

    inner class part1

--- src/main/java/org/master/Classes/InnerAbstractInterface/InnerClass/GearBox.java
+++ src/main/java/org/master/Classes/InnerAbstractInterface/InnerClass/GearBox.java
@@ -3,11 +3,18 @@ package org.master.Classes.InnerAbstractInterface.InnerClass;
 import java.util.ArrayList;
 
 public class GearBox {
-//    private ArrayList<Gear> gears;
+    private ArrayList<Gear> gears;
     private int maxGears;
     private int gearNumber = 0;
 
-    public GearBox(){
-
+    public GearBox(int maxGears){
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<>();
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
     }
+
+
+    GearBox mcLaren = new GearBox(6);
+
 }
