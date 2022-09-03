25a267efe74d4feb52b7db15c8668e2dc4366cbc
mustafaka <mustafakaratas4179@gmail.com>
2022-09-03 11:30:53 +0300

    abstract class mac 2

--- src/main/java/org/master/Classes/InnerAbstractInterface/AbstractClass/Challenge/MyLinkedList.java
+++ src/main/java/org/master/Classes/InnerAbstractInterface/AbstractClass/Challenge/MyLinkedList.java
@@ -34,7 +34,23 @@ public class MyLinkedList implements NodeList{
                     return true;
                 }
             } else if (comparison > 0) {
-                
+                //new item is less, insert before
+                if (currentItem.previous() != null){
+                    currentItem.previous().setNext(newItem);
+                    newItem.setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem);
+                    currentItem.setPrevious(newItem);
+                }else{
+                    // the node with a previous is the root
+                    newItem.setNext(this.root);
+                    this.root.setPrevious(newItem);
+                    this.root = newItem;
+                }
+                return true;
+            }else{
+                //equal
+                System.out.println(newItem.getValue() + " is already present, not added");
+                return false;
             }
         }
         //
@@ -43,11 +59,50 @@ public class MyLinkedList implements NodeList{
 
     @Override
     public boolean removeItem(ListItem item) {
+        if (item !=null){
+            System.out.println("Deleting item " + item.getValue());
+        }
+
+        ListItem currentItem = this.root;
+        while (currentItem != null){
+            int comparison = currentItem.compareTo(item);
+            if (comparison == 0){
+                //found the item to delete
+                if (currentItem == this.root){
+                    this.root = currentItem.next();
+                }else {
+                    currentItem.previous().setNext(currentItem.next());
+                    if (currentItem.next() != null){
+                        currentItem.next().setPrevious(currentItem.previous());
+                    }
+                }
+                return true;
+            } else if (comparison <0 ){
+                currentItem = currentItem.next();
+            } else {
+                //comparison > 0
+                //we are at an item greater than the one to be deleted, so the item is not in the list
+                return false;
+            }
+
+
+        }
+
+        //reached the end of the list
+        //
         return false;
     }
 
     @Override
     public void traverse(ListItem root) {
+        if (root == null){
+            System.out.println("the list is empty");
+        }else {
+            while (root != null) {
+                System.out.println(root.getValue());
+                root = root.next();
+            }
+        }
 
     }
 }
