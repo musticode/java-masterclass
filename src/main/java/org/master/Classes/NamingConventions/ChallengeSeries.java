bcc71df86a5fb0035e9811d0dd3a4a7093e5810a
mustidrummer <mustafakaratas4179@gmail.com>
2022-09-26 17:58:15 +0300

    final and static last video

---
+++ src/main/java/org/master/Classes/NamingConventions/ChallengeSeries.java
@@ -0,0 +1,27 @@
+package org.master.Classes.NamingConventions;
+
+public class ChallengeSeries {
+    public static long nSum(int n){
+        return (n*(n+1))/2;
+    }
+
+    public static long factorial(int n){
+        if (n == 0){
+            return 1;
+        } else if (n < 0) {
+            return -1;
+        } else {
+            long fact = 1;
+            for (int i = 0; i < n; i++){
+                fact *= i;
+            }
+            return fact;
+        }
+
+    }
+
+
+
+
+
+}
--- src/main/java/org/master/Classes/NamingConventions/Main.java
+++ src/main/java/org/master/Classes/NamingConventions/Main.java
@@ -1,5 +1,21 @@
 package org.master.Classes.NamingConventions;
 
+
+import org.master.Classes.NamingConventions.ScopeChallenge.X;
+
 public class Main {
+    public static void main(String[] args) {
+//        jdk.nashorn.internal.ir.Node node = null;
+
+
+        //scope challenge
+        X xS = new X(3);
+        xS.x();
+
+
+
+
+
+    }
 
 }
