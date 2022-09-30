7546b1b51a047a671fec89fc0cfecaafa76c2c23
mustidrummer <mustafakaratas4179@gmail.com>
2022-09-30 14:45:35 +0300

    collections 1

---
+++ src/main/java/org/master/Classes/Collections/Overview/Seat.java
@@ -0,0 +1,9 @@
+package org.master.Classes.Collections.Overview;
+
+public class Seat {
+    String seat;
+    public Seat(String seat){
+        this.seat = seat;
+    }
+
+}
---
+++ src/main/java/org/master/Classes/Collections/Overview/Theatre.java
@@ -0,0 +1,27 @@
+package org.master.Classes.Collections.Overview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Theatre {
+    private final String theatreName;
+    private List<Seat> seats = new ArrayList<>();
+
+    public Theatre(String theatreName, int numRows, int seatsPerRow){
+        this.theatreName = theatreName;
+        int lastRow = 'A' + (numRows -1);
+        for (char row = 'A'; row <= lastRow; row++){
+            for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++){
+                Seat seat = new Seat(row + String.format("%02d", seatNum));
+                seats.add(seat);
+            }
+        }
+
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+
+}
