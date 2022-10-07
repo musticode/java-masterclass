52bb0637e32eae0062ca564b17b2520dbf1e5bae
mustidrummer <mustafakaratas4179@gmail.com>
2022-10-07 16:19:24 +0300

    collections started-maps

--- src/main/java/org/master/Classes/Collections/Overview/Seat.java
+++ src/main/java/org/master/Classes/Collections/Overview/Seat.java
@@ -1,9 +1,48 @@
 package org.master.Classes.Collections.Overview;
 
-public class Seat {
-    String seat;
-    public Seat(String seat){
-        this.seat = seat;
+public class Seat implements Comparable<Seat>{
+
+    private String seatNumber;
+    private double price;
+    private boolean reserved = false;
+
+    public Seat(String seatNumber, double price){
+        this.seatNumber = seatNumber;
+        this.price = price;
+
     }
 
+    public String getSeatNumber(){
+        return seatNumber;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public boolean reserve(){
+        if (!this.reserved){
+            this.reserved = true;
+            System.out.println("Seat "+ seatNumber + " reserved");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean cancel(){
+        if (this.reserved){
+            this.reserved = false;
+            System.out.println("Seat "+ seatNumber + " cancelled");
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+    @Override
+    public int compareTo(Seat seat) {
+        return this.seatNumber.compareTo(seat.seatNumber);
+    }
 }
