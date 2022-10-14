5db3b0205d10610f605803b7d724257f34be1391
mustidrummer <mustafakaratas4179@gmail.com>
2022-10-14 17:47:04 +0300

    immutable classes office pc

--- src/main/java/org/master/Classes/Collections/AdventureGame/Main.java
+++ src/main/java/org/master/Classes/Collections/AdventureGame/Main.java
@@ -10,33 +10,50 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0, "You are sitting in front of com learning java0"));
-        locations.put(1, new Location(1, "You are sitting in front of com learning java1"));
-        locations.put(2, new Location(2, "You are sitting in front of com learning java2"));
-        locations.put(3, new Location(3, "You are sitting in front of com learning java3"));
-        locations.put(4, new Location(4, "You are sitting in front of com learning java4"));
-        locations.put(5, new Location(5, "You are sitting in front of com learning java5"));
+//        locations.put(0, new Location(0, "You are sitting in front of com learning java0"));
+//        locations.put(1, new Location(1, "You are sitting in front of com learning java1"));
+//        locations.put(2, new Location(2, "You are sitting in front of com learning java2"));
+//        locations.put(3, new Location(3, "You are sitting in front of com learning java3"));
+//        locations.put(4, new Location(4, "You are sitting in front of com learning java4"));
+//        locations.put(5, new Location(5, "You are sitting in front of com learning java5"));
 
-        locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("S", 3);
-        locations.get(1).addExit("E", 4);
-        locations.get(1).addExit("N", 5);
-        locations.get(1).addExit("Q", 0);
+        Map<String, Integer> tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 2);
+        tempExit.put("S", 3);
+        tempExit.put("E", 4);
+        tempExit.put("N", 5);
+        tempExit.put("Q", 0);
 
-        locations.get(2).addExit("N", 5);
-        locations.get(2).addExit("Q", 0);
-
-        locations.get(3).addExit("W", 1);
-        locations.get(3).addExit("Q", 0);
+        tempExit = new HashMap<>();
+        locations.put(6, new Location(6,"ssss", tempExit));
 
 
-        locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("W", 2);
-        locations.get(4).addExit("Q", 0);
 
-        locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("W", 2);
-        locations.get(5).addExit("Q", 0);
+
+
+
+
+
+//        locations.get(1).addExit("W", 2);
+//        locations.get(1).addExit("S", 3);
+//        locations.get(1).addExit("E", 4);
+//        locations.get(1).addExit("N", 5);
+//        locations.get(1).addExit("Q", 0);
+
+//        locations.get(2).addExit("N", 5);
+//        locations.get(2).addExit("Q", 0);
+//
+//        locations.get(3).addExit("W", 1);
+//        locations.get(3).addExit("Q", 0);
+//
+//
+//        locations.get(4).addExit("N", 1);
+//        locations.get(4).addExit("W", 2);
+//        locations.get(4).addExit("Q", 0);
+//
+//        locations.get(5).addExit("S", 1);
+//        locations.get(5).addExit("W", 2);
+//        locations.get(5).addExit("Q", 0);
 
 
 
