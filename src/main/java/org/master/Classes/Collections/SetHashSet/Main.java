db516346c6209c5ac00195cb00602205603cec51
mustafaka <mustafakaratas4179@gmail.com>
2022-10-17 11:08:34 +0300

    moon set

---
+++ src/main/java/org/master/Classes/Collections/SetHashSet/Main.java
@@ -0,0 +1,60 @@
+package org.master.Classes.Collections.SetHashSet;
+
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
+
+    public static void main(String[] args) {
+
+        HeavenlyBody temp = new HeavenlyBody("Mercury", 88);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Venus", 255);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Earth", 365);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        HeavenlyBody tempMoon = new HeavenlyBody("Moon", 27);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        tempMoon.addMoon(tempMoon);
+
+        //
+        System.out.println("Planets");
+        for(HeavenlyBody planet: planets){
+            System.out.println("\t"+ planet.getName());
+        }
+
+        //
+        HeavenlyBody body = solarSystem.get("Venus");
+        System.out.println("Moons of "+ body.getName());
+        for (HeavenlyBody jupiterMoon: body.getSatellites()){
+            System.out.println("\t"+ jupiterMoon.getName());
+        }
+
+        //
+        Set<HeavenlyBody> moons = new HashSet<>();
+        for (HeavenlyBody planet: planets){
+            moons.addAll(planet.getSatellites());
+        }
+
+        System.out.println("All moons");
+        for(HeavenlyBody moon: moons){
+            System.out.println("\t"+ moon.getName());
+        }
+
+
+    }
+}
+
+
